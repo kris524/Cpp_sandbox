c7208a2256e460c84d1024fc99db9fca047e47c9
Kris <kristian_dilov@abv.bg>
2023-06-04 21:54:47 +0100

    final

--- space_age/space_age.cpp
+++ space_age/space_age.cpp
@@ -2,53 +2,48 @@
 #include <iostream>
 
 namespace space_age {
-    const int earth_sec = 31557600;
-    const int mercury_sec = 0.2408467 * earth_sec;
-    const int venus_sec = 0.61519726 * earth_sec;
-    const int mars_sec = 1.8808158 * earth_sec;
-    const int jupyter_sec = 11.862615 * earth_sec;
-    const int saturn_sec = 1.8808158 * earth_sec;
-    const int uranus_sec = 84.016846 * earth_sec;
-    const int neptune_sec = 164.79132 * earth_sec;
+    constexpr double earth_sec = 31557600;
+    constexpr double mercury_sec = 0.2408467 * earth_sec;
+    constexpr double venus_sec = 0.61519726 * earth_sec;
+    constexpr double mars_sec = 1.8808158 * earth_sec;
+    constexpr double jupyter_sec = 11.862615 * earth_sec;
+    constexpr double saturn_sec = 29.447498 * earth_sec;
+    constexpr double uranus_sec = 84.016846 * earth_sec;
+    constexpr double neptune_sec = 164.79132 * earth_sec;
 
-   long space_age::space_age(long sec) : sec(sec) {}
+   space_age::space_age(long sec) : sec(sec) {}
 
    long long space_age::seconds() const {
     return (sec);
    }
 
    double space_age::on_earth() const {
-    return (sec/earth_sec)
+    return (sec/earth_sec);
    }
 
    double space_age::on_mercury() const {
-    return (sec/mercury_sec)
+    return (sec/mercury_sec);
    }
    double space_age::on_venus() const {
-    return (sec/venus_sec)
+    return (sec/venus_sec);
    }
    double space_age::on_mars() const {
-    return (sec/mars_sec)
+    return (sec/mars_sec);
    }
-   double space_age::on_jupyter() const {
-    return (sec/jupyter_sec)
+   double space_age::on_jupiter() const {
+    return (sec/jupyter_sec);
    }
    double space_age::on_saturn() const {
-    return (sec/saturn_sec)
+    return (sec/saturn_sec);
    }
    double space_age::on_uranus() const {
-    return (sec/uranus_sec)
+    return (sec/uranus_sec);
    }
-   double space_age::on_jupyter() const {
-    return (sec/jupyter_sec)
+   double space_age::on_neptune() const {
+    return (sec/neptune_sec);
    }
 
 
     
     
 }  // namespace space_age
-
-
-int main(){
-    std::cout << space_age::earth_sec << std::endl;
-}
