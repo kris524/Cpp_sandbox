023dc44a5aee6be53f4f6f8ad4d024edc1188a68
Kris <kristian_dilov@abv.bg>
2023-06-21 23:34:20 +0100

    quarter

---
+++ katas/quarter.cpp
@@ -0,0 +1,31 @@
+int quarter_of(int month){
+    
+
+    switch(month){
+        case 1:
+            return 1;
+        case 2:
+            return 1;
+        case 3:
+            return 1;
+        case 4:
+            return 2;
+        case 5:
+            return 2;
+        case 6:
+            return 2;
+        case 7:
+            return 3;
+        case 8:
+            return 3;
+        case 9:
+            return 3;
+        case 10:
+            return 4;
+        case 11:
+            return 4;
+        case 12:
+            return 4;
+    }
+
+}
