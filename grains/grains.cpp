fa4e461ad29595ea933fd3c1f71ef629e893c385
Kris <kristian_dilov@abv.bg>
2023-06-23 22:47:36 +0100

    grain

--- grains/grains.cpp
+++ grains/grains.cpp
@@ -1,8 +1,17 @@
 // so take the number given and do -1, then use this to do 2 to the power of it.
-
+#include <cmath>
 
 #include "grains.h"
 
-namespace grains {
-    
-}  // namespace grains
+namespace grains
+{
+
+    unsigned long long square(int x)
+    {
+        return pow(2, x - 1);
+    }
+    unsigned long long total()
+    {
+        return (1ULL << 63)*2 -1;
+    }
+} // namespace grains
