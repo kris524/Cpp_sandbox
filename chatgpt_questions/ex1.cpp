68c58340d40cb97e65bbfc3b99597d6e4aa4d545
Kris <kristian_dilov@abv.bg>
2023-06-26 22:10:39 +0100

    second try

--- chatgpt_questions/ex1.cpp
+++ chatgpt_questions/ex1.cpp
@@ -1,15 +1,44 @@
 #include<iostream>
 #include<algorithm>
+#include <stack>
 using namespace std;
 
 int* reverseArray(int* array, int size){
     
-    int values = *array;
-    int* res=NULL;
-    res = new int;
-    reverse(values, values+size);
-    *res = values;
+    int reversed_array[size];
+    
+    
+    int* res = reversed_array;
+
+    // int values = *array;
+    stack<int> stack;
+    for (int i=0; i<size; i++){
+        stack.push(array[i]);
+    }
+    // std::cout << stack;
+
+
+    int index = 0;
+    while (!stack.empty()){
+        reversed_array[index++]=stack.top();
+        stack.pop();
+    }
+    
     return res;
+
 
 }
 
+
+int main(){
+
+    int arr[] = {1,2,3,4,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    int* reversed = reverseArray(arr, size);
+
+    std::cout << "Reversed Array: ";
+    for (int i = 0; i < size; i++)
+        std::cout << reversed[i] << " ";
+    std::cout << "\n";
+}
