1c764ed35a24839ac504804b8dbe7ffab141b2ff
Kris <kristian_dilov@abv.bg>
2023-06-27 21:21:28 +0100

    feast

--- katas/triangle.cpp
+++ katas/triangle.cpp
@@ -8,6 +8,17 @@ public:
 
 
 bool feast(std::string beast, std::string dish){
-  //your code here
   
+    char firstChar = beast[0];
+    char lastChar = beast.back();
+
+    char firstfoodChar = dish[0];
+    char lastfoodChar = dish.back();
+    if (firstChar == firstfoodChar && lastfoodChar==lastChar){
+        return true;
+    }
+    else{
+        return false;
+    }
+
 }
