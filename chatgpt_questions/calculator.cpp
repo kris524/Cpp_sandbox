ce1a95a80d1baafd3d695483b32155455245ceed
Kris <kristian_dilov@abv.bg>
2023-06-28 08:13:22 +0100

    calculator

---
+++ chatgpt_questions/calculator.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+int main(){
+
+    std::cout<<"Enter two numbers"<< std::endl;
+    int x;
+    int y;
+    std::cin >> x;
+    std::cin >> y;
+
+    std::cout<<"Enter operand"<< std::endl;
+    char sign;
+    int result;
+    std::cin >> sign;
+
+    if (sign == '+'){
+        result = x+y;
+    }
+    if (sign == '-'){
+        result = x-y;
+    }
+    if (sign == '*'){
+        result = x*y;
+    }
+    if (sign == '/'){
+        result = x/y;
+    }
+
+    std::cout<<"Result"<< std::endl;
+    std::cout<< result<< std::endl;
+
+}
