74acbfa9030d69b099f60f44b11d5b15ca50382c
Kris <kristian_dilov@abv.bg>
2023-07-01 13:08:47 +0100

    swap

--- chatgpt_questions/swap.cpp
+++ chatgpt_questions/swap.cpp
@@ -1,12 +1,19 @@
 #include<iostream>
 
 
-void swap(int& arg1, int& arg2){
+void swap_by_reference(int& arg1, int& arg2){
     int temp = arg1;
     arg1 = arg2;
     arg2 = temp;
 
 }
+
+void swap_by_value(int arg1, int arg2){
+    int temp = arg1;
+    arg1 = arg2;
+    arg2 = temp;
+}
+
 
 int main(){
     int x = 2;
@@ -18,12 +25,25 @@ int main(){
     std::cout<<"START x "<<x<< std::endl;
     std::cout<<"START y "<<y<< std::endl;
     
-    swap(ref1, ref2);
+    swap_by_reference(ref1, ref2);
+   
 
     std::cout<<"END x "<<x<< std::endl;
     std::cout<<"END y "<<y<< std::endl;
 
+    int z = 2;
+    
 
+    int w = 3;
+
+    std::cout<<"START w "<<w<< std::endl;
+    std::cout<<"START z "<<z<< std::endl;
+    
+    swap_by_value(w, z);
+   
+
+    std::cout<<"END w "<<w<< std::endl;
+    std::cout<<"END z "<<z<< std::endl;
 
 }
 
