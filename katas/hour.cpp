62a3c6f36a7fe93bc4e7ee8a6720dad2eb4ec58b
Kris <kristian_dilov@abv.bg>
2023-07-02 11:19:41 +0100

    pipe_fix

--- katas/hour.cpp
+++ katas/hour.cpp
@@ -2,3 +2,21 @@ int past(int h, int m, int s) {
     int res = ((h*60*60) + (m*60) + s) * 1000;
     return res;
 }
+
+
+#include <vector>
+#include <numeric>
+
+std::vector<int> pipe_fix(const std::vector<int>& nums) {
+    // int start = nums.front();
+    // int end = nums.back();
+
+    auto start = *std::min_element(nums.begin(), nums.end());
+    auto end = *std::max_element(nums.begin(), nums.end());
+
+    std::vector<int> new_nums(1+end-start); // create a vector filled with 0 in that size
+
+    std::iota(new_nums.begin(), new_nums.end(), start);
+    return new_nums;
+
+}
