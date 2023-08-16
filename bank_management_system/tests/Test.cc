b9c5b4f226e1f5cd48c7280a9d8bd0c5ff4ebd31
Kris <kristian_dilov@abv.bg>
2023-08-16 23:35:50 +0100

    another testr

--- bank_management_system/tests/Test.cc
+++ bank_management_system/tests/Test.cc
@@ -6,5 +6,12 @@ TEST(MyClassTest, FirstTest){
     BankAccount test_ba("Josh", "1.1.1222", 4);
     int value = test_ba.show_statement();
     EXPECT_EQ(value, 4);
+}
+
+
+TEST(MyClassTest, SecondTest){
+    BankAccount test_ba("Josh", "1.1.1222", 4);
+    std::string value = test_ba.show_details();
+    EXPECT_EQ(value, "Josh 1.1.1222");
 
 }
