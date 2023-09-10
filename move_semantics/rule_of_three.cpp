b545ff46bd56eb9609a77b2eefd0d2e52a1ec175
Kris <kristian_dilov@abv.bg>
2023-09-10 15:03:28 +0100

    r of 3

--- move_semantics/rule_of_three.cpp
+++ move_semantics/rule_of_three.cpp
@@ -2,28 +2,58 @@
 
 class DynamicArray {
     public:
+        DynamicArray() : data(nullptr), size(0) {};
+
         DynamicArray(int size) : data(new int [size]), size(size) {};
       
         DynamicArray(const DynamicArray& other) {
-            cout << "Copy constructor called " << endl;
-            other.data = data;
-            other.size = size;
-        }
+            std::cout << "Copy constructor called " << std::endl;
+            data = new int [other.size];
+            size = other.size;
+            for (int i=0; i< other.size; i++){
+                data[i] = other.data[i];
+            }
+        };
         DynamicArray& operator=(const DynamicArray& other) {
-            cout << "Assignment operator called " << endl;
-        }
+            std::cout << "Assignment operator called " << std::endl;
+            if (this != &other){
+                delete[] data;
+                data = new int [other.size];
+                size = other.size;
+                for (int i=0; i< other.size; i++){
+                    data[i] = other.data[i];
+                }
+
+            }
+            return *this;
+
+        };
         ~DynamicArray() {
             delete[] data;
+        };
+
+        void printArray(){
+
+            for (int i=0; i< size; i++){
+                std::cout<< data[i] << " ";
+            }
         }
+        
 
     private:
         int* data;
         int size;
-}
+};
 
 int main(){
-    DynamicArray A = ()
-    A = B;
+    DynamicArray A(10);
+
+    A.printArray();
+
+    DynamicArray B;
+    // B = A;
     DynamicArray C = A;
+
+    C.printArray();
 
 }
