1bd715beebb70b35d631923ebf7af8d187e9bbd9
Kris <kristian_dilov@abv.bg>
2023-10-27 22:32:08 +0100

    finish singleton

--- singleton_pattern/singleton.cpp
+++ singleton_pattern/singleton.cpp
@@ -1,20 +1,9 @@
 #include <iostream> 
 #include <string>
 #include <mutex>
+#include <ctime>
 
 class Logger{
-
-
-    public:
-
-        static Logger getInstance() {
-            std::lock_guard<std::mutex> lock(mutex_);
-        }
-
-        void std::string 
-
-        Logger(Logger &const) = delete;
-        void operator=(const Logger &) = delete;
 
     private:
 
@@ -23,5 +12,54 @@ class Logger{
 
         Logger() {};
 
+    public:
 
+        static Logger* getInstance() {
+            std::lock_guard<std::mutex> lock(mutex_);
+            if (!instance){
+                instance = new Logger();
+            }
+            return instance;
+        }
+
+        void logInfo(const std::string& message ){
+            std::time_t now = time(0);
+            std::tm* localtime  = std::localtime(&now);  
+            std::cout << asctime(localtime) << "INFO: " << message << std::endl;
+        }
+
+        void logError(const std::string& message ){
+            std::time_t now = time(0);
+            std::tm* localtime  = std::localtime(&now);  
+            std::cout << asctime(localtime) << "ERROR: " << message << std::endl;
+        }
+
+
+
+        Logger(Logger &other) = delete;
+        void operator=(const Logger &) = delete;
+
+};
+
+Logger* Logger::instance{nullptr};
+std::mutex Logger::mutex_;
+
+
+int main(){
+    Logger* logger1 = Logger::getInstance();
+    Logger* logger2 = Logger::getInstance();
+
+    if (logger1 == logger2){
+        printf("True\n");
+    }
+    else{
+        printf("False\n");
+
+    }
+
+    logger1->logInfo("OMG price is cheap");
+
+    logger2->logError("This is bad");
+
+    // return 0;
 }
