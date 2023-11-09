e516eb5ba7ab4ba5a37c2f6a0ae889e4f0e7d589
Kris <kristian_dilov@abv.bg>
2023-11-09 23:51:59 +0000

    add the other strategies and context

--- patterns/strategy/strategy.cpp
+++ patterns/strategy/strategy.cpp
@@ -1,11 +1,47 @@
+#include <string>
+#include <iostream> 
 
 
 class PaymentStrategy {
 
     public: 
         virtual ~PaymentStrategy() = default;
-        virtual void payment_strat() =0;
+        virtual void payment_strat() {}
 
 };
 
 
+class CreditCardPayment : public PaymentStrategy{
+    
+    public:
+        std::string type() {
+            std::cout << "Credit Card Payment" << std::endl;
+        }
+
+};
+
+class PayPalPayment : public PaymentStrategy {
+    public:
+        std::string type() {
+            std::cout << "PayPal Payment" << std::endl;
+        }
+};
+
+
+class BitcoinPayment : public PaymentStrategy {
+    public:
+        std::string type() {
+            std::cout << "Bitcoin Payment" << std::endl;
+        }
+
+};
+
+class PaymentContext  {
+
+
+}
+
+
+int main() {
+    
+}
