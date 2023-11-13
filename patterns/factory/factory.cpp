4f931325d841dabb05a25d7cb101e814eae0ca7a
Kris <kristian_dilov@abv.bg>
2023-11-13 00:03:07 +0000

    refactor

--- patterns/factory/factory.cpp
+++ patterns/factory/factory.cpp
@@ -1,42 +1,57 @@
 #include <string>
 #include <iostream> 
+#include <vector>
+#include <string>
 
+class Document {
+    virtual std::vector<std::string> createPages() {}
 
+};
 
-class DocumentCreator{
-    public:
-        virtual Document create_document() {}
-
-        virtual void operation() {
-            
-        }
-
-}
-
-class ResumeFactory : public DocumentCreator{
-
-    Document create_document() override {
-        return Resume;
-    }
-
-}
-
-class ReportFactory : public DocumentCreator {
-    
-    Document create_document() override {
-        return Report;
-    }
-}
-
-
-class Document{
-
-}
 
 class Resume : public Document {
 
-}
+    std::vector<std::string> createPages() override {
+        return {"A", "B", "C"};
+    };
+};
 
 class Report : public Document {
+    std::vector<std::string> createPages() override {
+        return {"1", "3", "5"};
+    };
+};
+
+class DocumentCreator{
+    public:
+        virtual std::unique_ptr<Document> create_document() {}
+
+        virtual void operation() {
+            auto a = this->create_document();
+            *a.createPages()
+        }
+
+};
+
+class ResumeFactory : public DocumentCreator{
+
+    std::unique_ptr<Document> create_document() override {
+        return std::make_unique<Resume>();
+    }
+
+};
+
+class ReportFactory : public DocumentCreator {
+    
+    std::unique_ptr<Document> create_document() override {
+        return std::make_unique<Report>();
+    }
+};
+
+
+
+int main() {
+    ResumeFactory resume_facttory;
+
 
 }
