7fff7c636257759430e3759681c96eb60d8003d0
Kris <kristian_dilov@abv.bg>
2023-12-17 18:40:18 +0000

    custom wc command

---
+++ custom_wc/custom_wc.cpp
@@ -0,0 +1,28 @@
+#include<fstream>
+#include<iostream>
+
+using namespace std;
+// class ccwc {
+
+//     void parse(int argc, char*argv[]){
+//             if 
+//     }
+    
+// };
+
+
+int main(int argc, char* argv[]) {
+    printf("You have entered %d arguments:\n", argc);
+
+
+    if (argc>2){
+    string arg1 = argv[1];
+        if ( arg1 == "-c"){
+            printf("OMG\n");
+        }
+    }
+
+    // ifstream in_file("a.txt", ios::binary);
+    // in_file.seekg(0, ios::end);
+    // int file_size = in_file.tellg();
+}
