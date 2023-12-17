2d6e9167058bbcb4d9247560ff6a5fd9561bc500
Kris <kristian_dilov@abv.bg>
2023-12-17 20:41:30 +0000

    custom wc

--- custom_wc/custom_wc.cpp
+++ custom_wc/custom_wc.cpp
@@ -2,27 +2,19 @@
 #include<iostream>
 
 using namespace std;
-// class ccwc {
-
-//     void parse(int argc, char*argv[]){
-//             if 
-//     }
-    
-// };
-
 
 int main(int argc, char* argv[]) {
-    printf("You have entered %d arguments:\n", argc);
-
 
     if (argc>2){
-    string arg1 = argv[1];
-        if ( arg1 == "-c"){
-            printf("OMG\n");
+    string count_arg = argv[1];
+    string text_file = argv[2];
+        if ( count_arg == "-c"){
+            
+            ifstream in_file(text_file, ios::binary);
+            in_file.seekg(0, ios::end);
+            int file_size = in_file.tellg();
+            cout<< file_size << " " << text_file << endl;
         }
     }
 
-    // ifstream in_file("a.txt", ios::binary);
-    // in_file.seekg(0, ios::end);
-    // int file_size = in_file.tellg();
 }
